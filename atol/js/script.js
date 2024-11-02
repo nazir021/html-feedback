7c3ecb3885b483474df1de3ab8249e4604a9e91a
SabbirAtol <sabbiratol52@gmail.com>
2024-11-03 01:16:13 +0600

    by atol

---
+++ atol/js/script.js
@@ -0,0 +1,42 @@
+console.log('Hello World from Inside!')
+console.log('Atol')
+console.log('Student')
+var num = 52//use a number variable
+var name = 'Hossain'
+console.log(num)
+console.log(typeof(num))
+console.log(name)
+console.log(typeof(name))
+var aNumber = 10;
+console.log(aNumber)
+console.log(typeof(aNumber))
+var text =("cse department")
+console.log(text)
+console.log(typeof(text))//use a string 
+
+
+var isString =("10")
+console.log(String)
+console.log(typeof(String))
+
+
+
+var something ="Sabbir"
+console.log(something)
+console.log(typeof(something))
+something =10
+console.log(something)
+console.log(typeof(something))
+
+
+
+
+
+
+
+
+
+
+
+
+
